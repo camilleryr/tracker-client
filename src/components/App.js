a517fc3c180b2f9ebb7fbf4210095b3b70531192
Christopher Miller <christophermiller@Christophers-MacBook-Pro.local>
2018-03-21 09:27:32 -0500

    added heat map calander

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,12 @@
 import React, { Component } from "react";
 import AnimatedMap from "./AnimatedMap";
+import CalendarHeatmap from "react-calendar-heatmap";
 import "../css/App.css";
 
 class App extends Component {
   state = {
+    apiResponse: null,
+    values: [],
     cordArray: [[0,0]],
     geojson: {
       type: "FeatureCollection",
@@ -18,6 +21,30 @@ class App extends Component {
       ]
     }
   };
+
+  componentDidMount() {
+    fetch("http://danko.mit.edu/api/activities")
+      .then(x => x.json())
+      .then(y => {
+
+        let longestDistance = y.map(x => x.distance).sort((a,b) => b-a)[0]
+
+        let _values = y.map(x => {
+          let z = x.distance / longestDistance
+          return {
+            "date":x.startTime.substring(0,10),
+            "count": ((z < .2) ? 0
+                    : (z < .4) ? 1
+                    : (z < .6) ? 2
+                    : (z < .8) ? 3
+                               : 4),
+            "firebaseLocation": x.firebaseLocation
+          }})
+          
+        this.setState({ apiResponse: y, values:_values });
+      });
+
+  }
 
   setCordArray = (firebaseLocation = "-L75bqpa9c1m_92jzDHd") => {
     fetch(`https://trackmyrun-41804.firebaseio.com/${firebaseLocation}.json`)
@@ -61,8 +88,18 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        {/* <AnimatedMap cordArray={this.state.cordArray} geojson={this.state.geojson} timer={0}/>
-        <button onClick={this.handleClick}>Test</button> */}
+        <AnimatedMap cordArray={this.state.cordArray} geojson={this.state.geojson} timer={0}/>
+        {/* <button onClick={this.handleClick}>Test</button> */}
+        <CalendarHeatmap
+          onClick={value => {this.setCordArray(value.firebaseLocation)}}
+          values={this.state.values}
+          classForValue={value => {
+            if (!value) {
+              return "color-empty";
+            }
+            return `color-scale-${value.count}`;
+          }}
+        />
       </div>
     );
   }
