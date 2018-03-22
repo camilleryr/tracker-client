ce24768391f69ea2e94062f7ae5ef1305a1e1de3
Christopher Miller <christophermiller@Christophers-MacBook-Pro.local>
2018-03-22 10:15:06 -0500

    added logo

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "../css/Sidebar.css";
 
+
 const Sidebar = () => (
 
     <div className="sidebar">
 
         <div className="sidebar-block">
+            <img className="sidebar-logo" src={require("../assets/vectorpaint2.svg")} alt="logo" />    
         </div>
 
         <div className="sidebar-header">
